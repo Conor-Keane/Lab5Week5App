a8f3274ba4ee020a3d743d49a56252dd7e613bc8
conor-keane <G00391846@atu.ie>
2023-10-18 10:22:25 +0100

    Added FeignService class

--- src/main/java/ie/atu/week5blockingapplication/week5blockingapplication/FeignController.java
+++ src/main/java/ie/atu/week5blockingapplication/week5blockingapplication/FeignController.java
@@ -1,4 +1,14 @@
 package ie.atu.week5blockingapplication.week5blockingapplication;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FeignController {
+    private final FeignService feignService;
+
+    public FeignController(FeignService feignService) {
+        this.feignService = feignService;
+    }
+
+
 }
