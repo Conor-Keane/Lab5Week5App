25a9dcea0cb1cd24b874a1a5b16a590ac3653ea3
conor-keane <G00391846@atu.ie>
2023-10-18 10:16:53 +0100

    Added userId to TodoResponse

--- src/main/java/ie/atu/week5blockingapplication/week5blockingapplication/TodoResponse.java
+++ src/main/java/ie/atu/week5blockingapplication/week5blockingapplication/TodoResponse.java
@@ -1,4 +1,11 @@
 package ie.atu.week5blockingapplication.week5blockingapplication;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class TodoResponse {
+    @JsonProperty("userId")
+    private int userId;
+
 }
