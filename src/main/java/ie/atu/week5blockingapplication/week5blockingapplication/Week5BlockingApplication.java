cac3c8739f3b70ddc5f7a6fcee691ce769a0ac6f
conor-keane <G00391846@atu.ie>
2023-10-18 09:35:48 +0100

    Created week5 lab. added Rest Template to application class

---
+++ src/main/java/ie/atu/week5blockingapplication/week5blockingapplication/Week5BlockingApplication.java
@@ -0,0 +1,20 @@
+package ie.atu.week5blockingapplication.week5blockingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Week5BlockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5BlockingApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
